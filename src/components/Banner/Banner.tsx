8a07d8c9f0374910d1115e0bbb2cfcb2bfaf9b9d
Nikimad <PybePoiD@gmail.com>
2025-01-16 17:47:54 +0300

    Change src/components/Banner/Banner.tsx

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,8 @@
+import { Errors } from "@/lib/types";
 import Link from "next/link";
 import s from "./Banner.module.css";
 
-const Banner = () => (
+const Banner = ({ errors }: Readonly<{errors: Errors }>) => (
   <div className={s.banner}>
     <h1 className={s.banner__text}>ToDo</h1>
     <div className={s.banner__message}>
@@ -10,8 +11,8 @@ const Banner = () => (
         tasks you need to complete or things that you want to do
       </p>
       <p className={s.banner__text}>Here you can get one</p>
-      <Link href="/signin" className={`link ${s.banner__link}`}>
-        Create list
+      <Link href="/signin" className={`link ${s.banner__link}${errors ? " link_disabled" : ""}`}>
+        {!errors ? "Create list" : "Sorry, creating a list is currently unavailable" }
       </Link>
     </div>
   </div>
