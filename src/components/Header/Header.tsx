50417209e0dfc5b12a57ec46eefe6c1110662f4e
Nikimad <PybePoiD@gmail.com>
2025-01-16 17:19:34 +0300

    Change src/components/Header/Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import HeaderContainer from "./HeaderContainer";
 import s from "./Header.module.css";
 
 const Header = () => (
   <header className={s.header}>
-    <Link href="/signin" className="link">Sign in</Link>
-    <Link href="/signup" className="link">Sign up</Link>
+    <HeaderContainer>
+      <Link href="/signin" className="link">
+        Sign in
+      </Link>
+      <Link href="/signup" className="link">
+        Sign up
+      </Link>
+    </HeaderContainer>
   </header>
 );
 
