f2a44690c4e9cefee23aa4856ee20467ef5255b0
Nikimad <PybePoiD@gmail.com>
2025-01-18 22:55:32 +0300

    Change src/models/features/todos/index.ts

--- src/models/features/todos/index.ts
+++ src/models/features/todos/index.ts
@@ -1,15 +1,37 @@
-import { createSlice, createEntityAdapter, PayloadAction } from "@reduxjs/toolkit";
+import type { CaseReducer, PayloadAction } from "@reduxjs/toolkit";
 
-type TodoEntity = { id: number };//@duplicate
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+
+export type TodoEntity = {
+  id: string | number;
+  taskId: string | number;
+  text: string;
+  is_right: boolean;
+}; //@duplicate
+
+export type TodoStatus = {
+  status: string;
+} //@duplicate
+
+export type TodoPayloadAction = PayloadAction<TodoEntity>;
 
 export const todosAdapter = createEntityAdapter<TodoEntity>();
+
+const delegateActionToSaga: CaseReducer<
+  ReturnType<typeof todosAdapter.getInitialState>,
+  TodoPayloadAction
+> = (state) => state;
 
 export const todosSlice = createSlice({
   name: "todos",
   initialState: todosAdapter.getInitialState(),
   reducers: {
-    addTodo: (state, {}: PayloadAction<TodoEntity>) => state,
+    addTodo: delegateActionToSaga,
+    updateTodo: delegateActionToSaga,
+    removeTodo: delegateActionToSaga,
     addTodoSuccess: todosAdapter.addOne,
+    updateTodoSuccess: todosAdapter.updateOne,
+    removeTodoSuccess: todosAdapter.removeOne,
   },
 });
 
