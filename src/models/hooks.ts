05e7fd6359a28951299a90f1847cacdf52cd7d31
Nikimad <PybePoiD@gmail.com>
2025-01-19 19:47:45 +0300

    Change src/models/hooks.ts: add useAction

--- src/models/hooks.ts
+++ src/models/hooks.ts
@@ -1,7 +1,17 @@
+import { PayloadAction, PayloadActionCreator } from "@reduxjs/toolkit";
 import type { AppDispatch, AppStore, RootState } from ".";
 
+import { useCallback } from "react";
 import { useDispatch, useSelector, useStore } from "react-redux";
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppStore = useStore.withTypes<AppStore>();
+export const useAction = <Payload>(action: PayloadActionCreator<Payload>) => {
+  const dispatch = useAppDispatch();
+  const dispatchAction = useCallback(
+    (payload: Payload): PayloadAction<Payload> => dispatch(action(payload)),
+    [dispatch, action]
+  );
+  return dispatchAction;
+};
