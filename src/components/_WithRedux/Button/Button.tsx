6deabeddf9f527a006ba26c45a81c77d9da7d9db
Nikimad <PybePoiD@gmail.com>
2025-01-22 15:08:00 +0300

    Add src/components/_WithRedux/Button

---
+++ src/components/_WithRedux/Button/Button.tsx
@@ -0,0 +1,8 @@
+import s from "./Button.module.css";
+
+export interface ButtonProps
+  extends Readonly<React.ButtonHTMLAttributes<HTMLButtonElement>> {}
+
+const Button = ({ className, ...props }: ButtonProps) => <button className={className || s.button} {...props} />; 
+
+export default Button;
