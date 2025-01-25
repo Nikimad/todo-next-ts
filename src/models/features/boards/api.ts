b0e2e11311fb14c8bcbd19035416afc1008af25b
Nikimad <PybePoiD@gmail.com>
2025-01-25 21:53:44 +0300

    Change src/models/features/boards/api.ts

--- src/models/features/boards/api.ts
+++ src/models/features/boards/api.ts
@@ -1,30 +1,17 @@
 import type { FetchResponse } from "@/models/helpers";
 import type { BoardEntity, BoardStatus } from ".";
 
-import { _post, _patch, _delete } from "@/lib/helpers/_fetch";
-
-enum Endpoints {
-  Create,
-  Edit,
-  Delete,
-}
+import { _get, _post, _patch, _delete } from "@/lib/helpers/_fetch";
 
 export type BoardEntityResponse = FetchResponse<BoardEntity>;
-export type  BoardStatusResponse = FetchResponse<BoardStatus>;
+export type BoardStatusResponse = FetchResponse<BoardStatus>;
 
-const getEndpoint = (name: Endpoints, id?: string | number): string => {
-  const endpointsList: Record<Endpoints, string> = {
-    [Endpoints.Create]: `/tests`,
-    [Endpoints.Edit]: `/tests/${id}`,
-    [Endpoints.Delete]: `/tests/${id}`,
-  };
+export const boardsEndpoint = "tests";
 
-  return endpointsList[name];
-};
-
+export const getBoards = async () => await _get(boardsEndpoint);
 export const createBoard = async (payload: BoardEntity) =>
-  await _post(getEndpoint(Endpoints.Create), payload);
+  await _post(boardsEndpoint, payload);
 export const editBoard = async (payload: BoardEntity) =>
-  await _patch(getEndpoint(Endpoints.Edit, payload.id), payload);
+  await _patch(boardsEndpoint, payload);
 export const deleteBoard = async (payload: BoardEntity) =>
-  await _delete(getEndpoint(Endpoints.Delete, payload.id), payload);
+  await _delete(boardsEndpoint, payload);
