84d6cbd8debcb66157a55f17f5d98258fcc31cdb
Nikimad <PybePoiD@gmail.com>
2025-01-25 21:54:30 +0300

    Change src/models/features/tasks/api.ts

--- src/models/features/tasks/api.ts
+++ src/models/features/tasks/api.ts
@@ -2,29 +2,16 @@ import type { FetchResponse } from "@/models/helpers";
 import type { TaskEntity, TaskStatus } from ".";
 
 import { _post, _patch, _delete } from "@/lib/helpers/_fetch";
-
-enum Endpoints {
-  Create,
-  Edit,
-  Delete,
-}
+import { boardsEndpoint } from "../boards/api";
 
 export type TaskEntityResponse = FetchResponse<TaskEntity>;
 export type TaskStatusResponse = FetchResponse<TaskStatus>;
 
-const getEndpoint = (name: Endpoints, id: string | number): string => {
-  const endpointsList: Record<Endpoints, string> = {
-    [Endpoints.Create]: `/tests/${id}/questions`,
-    [Endpoints.Edit]: `/questions/${id}`,
-    [Endpoints.Delete]: `/questions/${id}`,
-  };
-
-  return endpointsList[name];
-};
+export const tasksEndpoint = "questions";
 
 export const createTask = async (payload: TaskEntity) =>
-  await _post(getEndpoint(Endpoints.Create, payload.boardId), payload);
+  await _post(`${boardsEndpoint}/${payload.boardId}/${tasksEndpoint}`, payload);
 export const editTask = async (payload: TaskEntity) =>
-  await _patch(getEndpoint(Endpoints.Edit, payload.id), payload);
+  await _patch(tasksEndpoint, payload);
 export const deleteTask = async (payload: TaskEntity) =>
-  await _delete(getEndpoint(Endpoints.Delete, payload.id), payload);
+  await _delete(tasksEndpoint, payload);
