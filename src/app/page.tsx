dedf4ad4f118e6d93630c3e4404fd5b1d6aeeacd
Nikimad <PybePoiD@gmail.com>
2025-01-25 21:45:30 +0300

    Change src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,12 @@
-export { default } from "@/components/Main";
+import BoardCreator from "@/components/_WithRedux/BoardCreator";
+import Boards from "@/components/_WithRedux/Boards";
+
+const BoardsPage = () => (
+  <>
+    <h2>Boards</h2>
+    <BoardCreator />
+    <Boards />
+  </>
+);
+
+export default BoardsPage;
