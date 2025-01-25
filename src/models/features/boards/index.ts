e8e5c47ad22326b593bf9a52bd4aae281788db29
Nikimad <PybePoiD@gmail.com>
2025-01-25 21:54:08 +0300

    Change src/models/features/boards/index.ts

--- src/models/features/boards/index.ts
+++ src/models/features/boards/index.ts
@@ -1,19 +1,35 @@
 import type { CaseReducer, PayloadAction } from "@reduxjs/toolkit";
 
 import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+import { UnnormolizeTaskEntity } from "../tasks";
+import { UnnormolizeState } from "@/models";
+
+export type UnnormolizeBoardEntity = {
+  id: string | number;
+  title: string;
+  questions: UnnormolizeTaskEntity[],
+  created_at: string;
+};
 
 export type BoardEntity = {
   id: string | number;
   title: string;
+  created_at: string;
 };
 
 export type BoardStatus = {
   status: string;
-} //@duplicate: TaskStatus, TodoStatus
+}; //@duplicate: TaskStatus, TodoStatus
 
 export type BoardPayloadAction = PayloadAction<BoardEntity>;
 
-export const boardsAdapter = createEntityAdapter<BoardEntity>();
+export const boardsAdapter = createEntityAdapter<BoardEntity>({
+  sortComparer: (a: BoardEntity, b: BoardEntity) => {
+    const firstdate = new Date(a.created_at);
+    const secdate = new Date(b.created_at);
+    return Number(secdate) - Number(firstdate);
+  }
+});
 
 const delegateActionToSaga: CaseReducer<
   ReturnType<typeof boardsAdapter.getInitialState>,
@@ -30,6 +46,13 @@ export const boardsSlice = createSlice({
     addBoardSuccess: boardsAdapter.addOne,
     updateBoardSuccess: boardsAdapter.updateOne,
     removeBoardSuccess: boardsAdapter.removeOne,
+    setBoards: (state, { payload: { boards } }: PayloadAction<UnnormolizeState>) => {
+      const normolizedBoards: BoardEntity[] = boards.map(
+        ({ title, id, created_at }) => ({ title, id, created_at })
+      );
+      
+      boardsAdapter.setAll(state, normolizedBoards);
+    },
   },
 });
 
