c1743e0f74cb2fe69808cea8ffd87ef193e0119c
Nikimad <PybePoiD@gmail.com>
2025-01-27 15:46:48 +0300

    Change src/models/features/authorization/sagas.ts

---
+++ src/models/features/authorization/sagas.ts
@@ -0,0 +1,22 @@
+import { call, put, takeEvery } from "redux-saga/effects";
+import { authorizationActions, AuthorizationDataPayloadAction } from ".";
+import { sign, logout, UserResponse, StatusResponse } from "./api";
+import { statusActions } from "../status";
+
+function* signSaga({ payload }: AuthorizationDataPayloadAction) {
+  const [errors, user]: UserResponse = yield call(sign, payload);
+  if (user) yield put(authorizationActions.setUser(user));
+  if (errors) yield put(authorizationActions.setErrors(errors));
+}
+
+function* logoutSaga() {
+  const [errors, status]: StatusResponse = yield call(logout);
+  if (status?.success) yield put(authorizationActions.setUser(null));
+  if (errors) yield put(statusActions.setStatusReject());
+}
+
+export function* authorizationWatcherSaga() {
+  yield takeEvery(authorizationActions.signin.match, signSaga);
+  yield takeEvery(authorizationActions.signup.match, signSaga);
+  yield takeEvery(authorizationActions.logout, logoutSaga);
+}
