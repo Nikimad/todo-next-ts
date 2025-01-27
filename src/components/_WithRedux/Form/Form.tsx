177d286ac43a2097165b8a687e7b20dbcf99302c
Nikimad <PybePoiD@gmail.com>
2025-01-27 15:34:55 +0300

    Change src/components/_WithRedux/Form

--- src/components/_WithRedux/Form/Form.tsx
+++ src/components/_WithRedux/Form/Form.tsx
@@ -1,4 +1,4 @@
-export type FormProps = Readonly<React.FormHTMLAttributes<HTMLFormElement>>;
+export type FormProps = Readonly<React.ComponentProps<"form">>;
 
 const Form = ({ className, ...props }: FormProps) => (
   <form className={`styled-wrapper ${className || ""}`} {...props} />
