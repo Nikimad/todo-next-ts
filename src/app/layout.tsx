2fd953443edd245fde23aaceb195d564e1fcd987
Nikimad <PybePoiD@gmail.com>
2025-01-27 15:38:19 +0300

    Change src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Funnel_Sans } from "next/font/google";
 import "@/global.css";
-import Link from "next/link";
 import ReduxProvider from "@/components/Redux";
 import Header from "@/components/_WithRedux/Header";
 import Main from "@/components/_WithRedux/Main";
+import Nav from "@/components/_WithRedux/Nav";
 
 const hankenGrotesk = Funnel_Sans({
   subsets: ["latin"],
@@ -25,8 +25,7 @@ export default function RootLayout({
       <body className={`body ${hankenGrotesk.className}`}>
         <ReduxProvider>
           <Header>
-            <Link href="/signin">Sign in</Link>
-            <Link href="/signup">Sign up</Link>
+            <Nav />
           </Header>
           <Main>{children}</Main>
         </ReduxProvider>
