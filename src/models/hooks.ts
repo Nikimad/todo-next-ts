a1cdc205ed9a28284518c1952b337625e17d7bf5
Nikimad <PybePoiD@gmail.com>
2025-01-28 16:42:47 +0300

    Change src/models/hooks.ts

--- src/models/hooks.ts
+++ src/models/hooks.ts
@@ -7,10 +7,12 @@ import { useDispatch, useSelector, useStore } from "react-redux";
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppStore = useStore.withTypes<AppStore>();
-export const useAction = <Payload>(action: PayloadActionCreator<Payload>) => {
+export const useAction = <Payload>(action?: PayloadActionCreator<Payload>) => {
   const dispatch = useAppDispatch();
   const dispatchAction = useCallback(
-    (payload: Payload): PayloadAction<Payload> => dispatch(action(payload)),
+    (payload: Payload): PayloadAction<Payload> | void => {
+      if (action) return dispatch(action(payload));
+    },
     [dispatch, action]
   );
   return dispatchAction;
