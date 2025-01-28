43033b2d1c8a0f4f3de4bf6e0db07a35b3e15961
Nikimad <PybePoiD@gmail.com>
2025-01-28 16:35:14 +0300

    Change src/components/AuthorizaedNav

--- src/components/AuthorizedNav/AuthorizedNav.tsx
+++ src/components/AuthorizedNav/AuthorizedNav.tsx
@@ -1,9 +1,28 @@
+import Link from "next/link";
 import Button from "../Button";
+import s from "./AuthorizedNav.module.css";
 
 type AuthorizedNavProps = {
-    onLogout: () => void;
+  onLogout: () => void;
+  params: { boardId: string; taskId: string };
 };
 
-const AuthorizedNav = ({ onLogout }: Readonly<AuthorizedNavProps>) => <Button onClick={onLogout}>Log out</Button>;
+const AuthorizedNav = ({ onLogout, params }: Readonly<AuthorizedNavProps>) => (
+  <nav className={s.nav}>
+    <div className={s.nav__path}>
+      {params.boardId && (
+        <Link href="/" className="link">
+          Boards
+        </Link>
+      )}
+      {params.taskId && (
+        <Link href={`/${params.boardId}`} className="link">
+          Tasks
+        </Link>
+      )}
+    </div>
+    <Button onClick={onLogout}>Log out</Button>
+  </nav>
+);
 
 export default AuthorizedNav;
--- src/components/AuthorizedNav/AuthorizedNavContainer.tsx
+++ src/components/AuthorizedNav/AuthorizedNavContainer.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { authorizationActions } from "@/models/features/authorization";
-import { useAction } from "@/models/hooks";
-import AuthorizedNav from "./AuthorizedNav";
 import { useCallback } from "react";
+import { useParams } from "next/navigation";
+import { useAction } from "@/models/hooks";
+import { authorizationActions } from "@/models/features/authorization";
+import AuthorizedNav from "./AuthorizedNav";
 
 const AuthorizedNavContainer = () => {
+    const params = useParams<{ boardId: string; taskId: string }>();
     const logout = useAction(authorizationActions.logout);
     const handleLogout = useCallback(() => logout(undefined), [logout]);
 
-    return <AuthorizedNav onLogout={handleLogout} />
+    return <AuthorizedNav onLogout={handleLogout} params={params} />
 };
 
 export default AuthorizedNavContainer;
