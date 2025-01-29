aeebbaa3596a068fc84c90ad4a976d10825bbf25
Nikimad <PybePoiD@gmail.com>
2025-01-29 10:01:27 +0300

    Change src/models/features/boards/sagas.ts

--- src/models/features/boards/sagas.ts
+++ src/models/features/boards/sagas.ts
@@ -1,10 +1,22 @@
 import type { BoardPayloadAction } from ".";
-import type { BoardEntityResponse, BoardStatusResponse } from "./api";
+import type { BoardEntityResponse, BoardStatusResponse, UnnormalDataResponse } from "./api";
 
 import { call, put, takeEvery } from "redux-saga/effects";
 import { boardsActions } from ".";
-import { createBoard, editBoard, deleteBoard } from "./api";
+import { createBoard, editBoard, deleteBoard, getBoards } from "./api";
+import normalizeData from "@/lib/helpers/normalizeData";
 import { tasksActions } from "../tasks";
+import { todosActions } from "../todos";
+
+function* getBoardsSaga() {
+  const [errors, data]: UnnormalDataResponse = yield call(getBoards);
+  if (data){
+    const nomalizedData = normalizeData(data);
+    yield put(boardsActions.setBoards(nomalizedData.boards));
+    yield put(tasksActions.setTasks(nomalizedData.tasks));
+    yield put(todosActions.setTodos(nomalizedData.todos));
+  }
+}
 
 function* addBoardSaga({ payload }: BoardPayloadAction) {
   const [errors, newBoard]: BoardEntityResponse = yield call(createBoard, payload);
@@ -33,6 +45,7 @@ function* removeBoardSaga({ payload }: BoardPayloadAction) {
 }
 
 export function* boardsWatcherSaga() {
+  yield takeEvery(boardsActions.getBoards, getBoardsSaga);
   yield takeEvery(boardsActions.addBoard, addBoardSaga);
   yield takeEvery(boardsActions.updateBoard, updateBoardSaga);
   yield takeEvery(boardsActions.removeBoard, removeBoardSaga);
