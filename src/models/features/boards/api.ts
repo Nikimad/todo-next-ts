aeebe9f2cfb37b8e9dd5afd893aa7769e445841d
Nikimad <PybePoiD@gmail.com>
2025-01-29 10:01:01 +0300

    Change src/models/features/boards/api.ts

--- src/models/features/boards/api.ts
+++ src/models/features/boards/api.ts
@@ -1,15 +1,17 @@
 import type { FetchResponse } from "@/models/helpers";
 import type { BoardEntity, BoardStatus } from ".";
-import { UnnormalBoardEntity } from "@/lib/helpers/normalizeData";
+import { UnnormalBoardEntity, UnnormalData } from "@/lib/helpers/normalizeData";
 
 import { _get, _post, _patch, _delete } from "@/lib/helpers/_fetch";
 
+export type UnnormalDataResponse = FetchResponse<UnnormalData>;
 export type BoardEntityResponse = FetchResponse<UnnormalBoardEntity>;
 export type BoardStatusResponse = FetchResponse<BoardStatus>;
 
 export const boardsEndpoint = "tests";
 
-export const getBoards = async () => await _get(boardsEndpoint);
+export const getBoards = async () =>
+  await _get(boardsEndpoint, new URLSearchParams({ per: "1000" }));
 export const createBoard = async (payload: BoardEntity) =>
   await _post(boardsEndpoint, payload);
 export const editBoard = async (payload: BoardEntity) =>
