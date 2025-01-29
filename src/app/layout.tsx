f835681db21ce8a8fa6d478b24eb8f417bfabefc
Nikimad <PybePoiD@gmail.com>
2025-01-29 10:06:05 +0300

    Change src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import ReduxProvider from "@/components/Redux";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
 import Nav from "@/components/Nav";
-import Banner from "@/components/Banner";
 
 const hankenGrotesk = Funnel_Sans({
   subsets: ["latin"],
